1b9bb43723bb2209bf19ef16a3be94d7f86e9c2c
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-05-01 15:06:13 +0530

    Added DTO's

---
+++ EMS/Shared/EmployeeDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class EmployeeDto_Admin : EmployeeDto
+    {
+        public decimal Salary { get; set; }
+    }
+}
